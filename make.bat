f71ddbf0825e82b5c483a733f4066ed896d8d180
Alejandro <alejandrotarazona@gmail.com>
2015-07-04 22:31:36 -0430

    Anexo1
    Corregido y con 1 anexo

---
+++ make.bat
@@ -0,0 +1,9 @@
+xelatex InformeSC
+echo "Primera Compilacion lista"
+bibtex InformeSC
+echo "Compilacion de Bib lista"
+pause
+xelatex InformeSC
+echo "Segunda Compilacion lista"
+xelatex InformeSC
+echo "Ultima Compilacion lista"
